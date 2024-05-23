8ce200636555510676d9c4d62e6c6f2029af4d33
PID- Tina96_Chen <tina96_chen@mail.chungyo.net>
2024-05-23 11:49:26 +0800

    px轉rem檔案

---
+++ toRem/index.py
@@ -0,0 +1,38 @@
+# 從px轉rem
+# input txt file output txt file
+import re
+
+def px_to_rem(value):
+    base = 14
+    rem_value = round((float(value) / base),2)
+    return str(rem_value)+'rem'
+
+def lineHandler(css_string):
+    if line.strip().startswith(('border-radius', 'border', 'filter')):
+        return line
+
+    pattern = re.compile(r'(\d+)px')
+    return pattern.sub(lambda match: px_to_rem(match.group(1)), css_string)
+
+
+path = 'text.txt'
+output = 'output.txt'
+output_file = open(output, 'w')
+f = open(path, 'r')
+
+for line in f.readlines():
+    textTrim=line.strip()
+
+    if len(textTrim)!=0:
+        result =lineHandler(textTrim)
+        output_file.write(result + '\n')
+        print(result)
+
+f.close()
+output_file.close()
+
+
+
+
+
+
