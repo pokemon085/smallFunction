935147f93481535132697bd408a6ce134b1c83da
Tina96_Chen <tina96_chen@mail.chungyo.net>
2024-12-26 11:08:56 +0800

    色碼對應功能

---
+++ toColorMap/colorMap.py
@@ -0,0 +1,30 @@
+baseColor= {
+  '#2665aa':'$primary-1',
+  '#4890f7':'$primary-2',
+  '#b8ddff':'$primary-3',
+  '#e3eeff':'$primary-4',
+  '#f6f9ff':'$primary-5',
+  '#404e67':'$text-1',
+  '#606266':'$text-2',
+  '#818b9d':'$text-3',
+  '#a7aebb':'$text-4',
+  '#fff':'$white',
+  '#ff6275':'$danger',
+  '#febf05':'$warning',
+  '#06b76c':'$success',
+  '#22c2dc':'$freeze',
+  '#86b251':'$online',
+  '#b176c4':'$suspended',
+  '#fa8652':'$locked'
+}
+
+backgroundColor= {
+    '#404e67':'$background-1',
+    '#a7aebb':'$background-2',
+    '#dae1e8':'$background-3',
+    '#e3ebf3':'$background-4',
+    '#f5f7fa':'$background-5',
+    '#e2e4e8':'$background-6',
+    '#d0d7e7':'$background-7',
+    '#fff':'$white',
+}
